c83042351b6b4ea7caa15ed40ae2a591c0b49135
hsaigusa <hirokazu.k.saigusa@gmail.com>
2021-09-08 23:32:25 +1000

    Describe added

---
+++ Describe.py
@@ -0,0 +1,24 @@
+import arcpy
+
+# Data to be described
+dElement = r"D:\EsriTraining_Past\SADistance\SanDiego.gdb"
+desc = arcpy.Describe(dElement)
+
+# Print the name of the data elements, including Child data
+print(f"Describing {dElement}...")
+print("Name:        " + desc.name)
+print("Dtype:     " + desc.dataType)
+print("CatalogPath:        " + desc.catalogPath)
+print("Children:")
+for child in desc.children:
+    if child.dataType == "FeatureDataset":
+        pass
+    if hasattr(child, "ShapeType"):
+        print(f"    with Extent: {child.extent}")
+    else:
+        print(f"  {child.name} is a {child.dataType}")
+    print("   and Fields:")
+    for field in child.fields:
+        print(f"  {field.name} is a {field.type}")
+
+print("\nScript finished")
--- create_gdb_change_source.py
+++ create_gdb_change_source.py
@@ -1,5 +1,7 @@
 import arcpy
 
+arcpy.env.overwriteOutput = True
+
 # List names of layers
 aprx = arcpy.mp.ArcGISProject(r"D:\LPA\Project\MyProject\MyProject.aprx")
 for m in aprx.listMaps():
@@ -10,6 +12,9 @@ del aprx
 
 # Create a new gdb to send layers
 arcpy.CreateFileGDB_management("D:\LPA\Project\MyProject", "sd_package")
+
+arcpy.FeatureClassToFeatureClass_conversion()
+
 # Check the projection of all layers
 
 # Change the projection of all layers
